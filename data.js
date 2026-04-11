572cad30cb341bdc0c6f626c8d8f11ca7710d2c1
azo73 <crues7374@gmail.com>
2026-04-11 14:15:01 +0300

    Update data.js

--- data.js
+++ data.js
@@ -79,7 +79,7 @@ const vocabularyData = [
     { "id": 67, "category": "Personal", "german": "Oberarzt", "type": "Der Nomen", "turkish": "Uzman doktor", "example": "Der Oberarzt macht heute Dienst." },
     { "id": 68, "category": "Personal", "german": "Chefarzt", "type": "Der Nomen", "turkish": "Başhekim", "example": "Der Chefarzt ist im Operationssaal." },
     { "id": 69, "category": "Personal", "german": "Stationsleitung", "type": "Die Nomen", "turkish": "Servis sorumlusu", "example": "Sprechen Sie mit der Stationsleitung." },
-    { "id": 70, "category": "Personal", "german": "Physiotherapeut", "type": "Der Nomen", "turkish": "Fizyoterapist", "example": "Der Physiotherapeut geliyor saat 10'da." },
+    { "id": 70, "category": "Personal", "german": "Physiotherapeut", "type": "Der Nomen", "turkish": "Fizyoterapist", "example": "Der Physiotherapeut kommt um 10 Uhr." },
     { "id": 71, "category": "Personal", "german": "Logopäde", "type": "Der Nomen", "turkish": "Konuşma terapisti", "example": "Der Logopäde hilft beim Schlucken." },
     { "id": 72, "category": "Personal", "german": "Hebamme", "type": "Die Nomen", "turkish": "Ebe", "example": "Die Hebamme ist im Kreißsaal." },
     { "id": 73, "category": "Personal", "german": "Auszubildende", "type": "Die Nomen", "turkish": "Stajyer", "example": "Unsere Auszubildende hilft Ihnen." },
