cbba529040e64015423608f8bc93afbfdb4d7370
azo73 <crues7374@gmail.com>
2026-04-11 19:53:17 +0300

    Update data.js,

--- data.js
+++ data.js
@@ -135,7 +135,7 @@ const vocabularyData = [
     { "id": 115, "category": "Dialoge", "german": "Warten Sie bir an.", "type": "Satz", "turkish": "Bir dakika bekleyin.", "example": "Ich bin gleich wieder da." },
     { "id": 116, "category": "Dialoge", "german": "Der Arzt kommt gleich.", "type": "Satz", "turkish": "Doktor birazdan geliyor.", "example": "Bitte gedulden Sie sich einen Moment." },
     { "id": 117, "category": "Dialoge", "german": "Haben Sie Allergien?", "type": "Satz", "turkish": "Alerjiniz var mı?", "example": "Haben Sie Allergien gegen Penicillin?" },
-    { "id": 118, "category": "Dialoge", "german": "Gute Besserung!", "type": "Satz", "turkish": "Geçmiş olsun!", "example": "Güle güle ve gute Besserung!" },
+    { "id": 118, "category": "Dialoge", "german": "Gute Besserung!", "type": "Satz", "turkish": "Geçmiş olsun!", "example": "Auf Wiedersehen und gute Besserung!" },
     { "id": 119, "category": "Dialoge", "german": "Soll ich das Fenster kapatayım mı?", "type": "Satz", "turkish": "Pencereyi kapatayım mı?", "example": "Ist es Ihnen çok soğuk?" },
     { "id": 120, "category": "Dialoge", "german": "Was kann ich für Sie tun?", "type": "Satz", "turkish": "Sizin için ne yapabilirim?", "example": "Brauchen Sie daha bir şey?" },
     { "id": 121, "category": "Dialoge", "german": "Haben Sie Stuhlgang gehabt?", "type": "Satz", "turkish": "Tuvalete çıktınız mı?", "example": "Haben Sie bugün zaten Stuhlgang gehabt?" },
